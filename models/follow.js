4caabe9ee3bad51d95268e4ab94eaed0ff669fc0
ckuang <ckuang8@gmail.com>
2017-02-02 11:00:09 -0500

    solutions

--- models/follow.js
+++ models/follow.js
@@ -4,6 +4,8 @@ module.exports = function(sequelize, DataTypes) {
   var Follow = sequelize.define("Follow", {}, {
     classMethods: {
       associate: function(models) {
+        Follow.belongsTo(models.User, {as: 'follower'});
+        Follow.belongsTo(models.User, {as: 'followed'});
       }
     }
   });
--- models/userproject.js
+++ models/userproject.js
@@ -1,9 +1,13 @@
 "use strict";
 
 module.exports = function(sequelize, DataTypes) {
-  var UserProject = sequelize.define("UserProject", {}, {
+  var UserProject = sequelize.define("UserProject", {
+    
+  }, {
     classMethods: {
       associate: function(models) {
+        UserProject.belongsTo(models.User)
+        UserProject.belongsTo(models.Project)
       }
     }
   });
